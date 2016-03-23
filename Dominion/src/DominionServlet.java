bcd8ef47ddf706c468846097cf7584bdd74834b3
trimonvl <trimon@telenet.be>
2016-03-23 09:35:01 +0100

    Test
    Added hello world test

--- Dominion/src/DominionServlet.java
+++ Dominion/src/DominionServlet.java
@@ -1,6 +1,6 @@
 
 
-import java.io.IOException;
+import java.io.*;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -25,7 +25,11 @@ public class DominionServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	    response.setContentType("text/html");
+	    		    response.setCharacterEncoding("utf-8");
+	    		    PrintWriter out = response.getWriter();
+	    		    out.println("<html><body>Hello World.</body></html>");
+	    		      System.out.println("ik heb gewerkt...");
 	}
 
 	/**
